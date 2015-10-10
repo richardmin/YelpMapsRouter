8e8e083072c5fe35f04bb525c1b3f741c7c4e772
Grace Tsang <gracetsang1996@gmail.com>
2015-10-10 16:09:17 -0700

    something

--- Frontend/js/searchBar.js
+++ Frontend/js/searchBar.js
@@ -1,10 +1,8 @@
-//
 var API_HOST = 'api.yelp.com'
 var DEFAULT_TERM = 'dinner'
 var DEFAULT_LOCATION = 'San Francisco, CA'
 var SEARCH_LIMIT = 3
 var SEARCH_PATH = '/v2/search/'
-var BUSINESS_PATH = '/v2/business/'
 
 var CONSUMER_KEY = "Emj2tJMe8PC1HOvsCCw_UA";
 var CONSUMER_SECRET = "zIcn9nu2qfpIZQP8xdSguTsx59s";
@@ -13,15 +11,15 @@ var TOKEN_SECRET = "kvYgcIC7raNCHWhEfBqSZInzGuQ";
 
 function yelpInit(){
 	var yelp = require("../index").createClient({
-	  consumer_key: process.env.CONSUMER_KEY,
-	  consumer_secret: process.env.CONSUMER_SECRET,
-	  token: process.env.TOKEN,
-	  token_secret: process.env.TOKEN_SECRET,
+	  consumer_key: CONSUMER_KEY,
+	  consumer_secret: CONSUMER_SECRET,
+	  token: TOKEN,
+	  token_secret: TOKEN_SECRET,
 	  ssl: true
 	});
 }
 
-function search(){
+function search(/*Theoretically should have arguments*/){
 	yelp.search({term: "food", location: "Montreal"}, function(error, data) {
 	  console.log(error);
 	  console.log(data);
